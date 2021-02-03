146313bd44e82a9378c82d7feb2923bd2c4c4e23
QQQQQby <yx41_qby@126.com>
2021-02-04 01:36:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simulation Games
+
+This is an Android APP with some simulation games such as  **Conway's Game Of Life** and **Chaos Game**
+
+## Introduction
+
+- [Conway's Game Of Life](https://en.wikipedia.org/wiki/Conway's_Game_of_Life)
+
+- [Chaos Game](https://en.wikipedia.org/wiki/Chaos_game)
---
+++ app/src/main/java/com/byqi/simulationgames/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.byqi.simulationgames.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.byqi.simulationgames.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
